cff9987a43900856291ec7471c8f67fa0842bd9f
xgl <2608543868@qq.com>
2022-05-23 15:35:04 +0800

    netty

---
+++ netty/src/main/java/com/itjoin/pro_netty/util/PropertyUtil.java
@@ -0,0 +1,30 @@
+package com.itjoin.pro_netty.util;
+
+import java.util.Properties;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class PropertyUtil {
+    private static final Properties serverProperties = new Properties();
+    protected static final Logger LOGGER = LoggerFactory.getLogger(PropertyUtil.class);
+
+    static {
+        try {
+            serverProperties.load(PropertyUtil.class.getResourceAsStream("/server.properties"));
+            LOGGER.error("加载配置文件成功========");
+        } catch (Exception e) {
+            e.printStackTrace();
+            LOGGER.error("加载配置文件失败========");
+
+        }
+    }
+
+    public static String getString(String key) {
+        return serverProperties.getProperty(key);
+    }
+
+    public static Integer getInt(String key) {
+        return Integer.valueOf(serverProperties.getProperty(key));
+    }
+}
